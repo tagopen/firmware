437a68ac517233ab3e1ce12a78f86ea77c911d1b
Security2431 <Artem2431@gmail.com>
2017-01-27 16:40:24 +0200

    matrix modal

--- js/main.js
+++ js/main.js
@@ -28,3 +28,54 @@ inp.change(function(){
 $( window ).resize(function(){
   $( ".upload__input" ).triggerHandler( "change" );
 });
+// Bootstra-selector init
+$('.selectpicker').selectpicker({
+  size: 4
+});
+
+$(document).ready(function() {
+  $('.tabs__controls-link').click(function(e) {
+    e.preventDefault();
+    var $item = $(this).closest('.tabs__controls-item'),
+        $contentItem = $('.tabs__item'),
+        $itemPosition = $(this).attr('href');
+
+    $contentItem.filter($itemPosition)
+    .add($item)
+    .addClass('active')
+    .siblings()
+    .removeClass('active');
+  });
+});
+
+// Without JQuery
+var slider1 = new Slider('#ex1', {
+  formatter: function(value) {
+    return 'Current value: ' + value;
+  }
+});
+var slider2 = new Slider('#ex2', {
+  formatter: function(value) {
+    return 'Current value: ' + value;
+  }
+});
+var slider3 = new Slider('#ex3', {
+  formatter: function(value) {
+    return 'Current value: ' + value;
+  }
+});
+var slider4 = new Slider('#ex4', {
+  formatter: function(value) {
+    return 'Current value: ' + value;
+  }
+});
+var slider5 = new Slider('#ex5', {
+  formatter: function(value) {
+    return 'Current value: ' + value;
+  }
+});
+var slider6 = new Slider('#ex6', {
+  formatter: function(value) {
+    return 'Current value: ' + value;
+  }
+});
